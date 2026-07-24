d3dfedebb0f041f568171302418bf5dbab98c08d
刘赫渊 <1664304513@qq.com>
2026-07-24 09:52:34 +0800

    feat: 完成首页开发，添加交互功能

--- my-blog/script.js
+++ my-blog/script.js
@@ -1,3 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('页面已加载');
 });
+
+// 获取按钮元素
+const btn = document.getElementById('clickMe');
+
+// 添加点击事件
+btn.addEventListener('click', function() {
+    alert('恭喜你！你已经成功运行了 JavaScript 代码！');
+    // 改变背景颜色
+    document.body.style.backgroundColor = '#f0f8ff';
+});
